c9940b5d6808415b65099a8bbbd776d7a0ff3493
Кристина Бондаренко <bondarenko.k11@gmail.com>
2021-06-16 13:03:32 +0700

    Добавьте файлы проекта.

---
+++ Morse/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using Morse_Translator;
+
+namespace Morse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Select Translation: \n1: from English to Morse \n2: from Morse to English \n3: from Russian to Morse \n4: from Morse to Russian");
+            int i = 0;
+            if (!Int32.TryParse(Console.ReadLine(), out i))
+            {
+                Console.WriteLine("Are you sure that you understand me?");
+            }
+            else if (i > 4 || i <= 0)
+            {
+                Console.WriteLine("There is no such option");
+            }
+            else
+            {
+                using (StreamReader fReader = new StreamReader("read.txt"))
+                {
+                    if (i == 1)
+                    {
+                        Translatоr.SetDictionary("eng"); 
+                        Translatоr.TranslateToMorse(fReader);
+                    }
+                    if (i == 2)
+                    {
+                        Translatоr.SetDictionary("eng");
+                        Translatоr.TranslateFromMorse(fReader);
+                    }
+                    if (i == 3)
+                    {
+                        Translatоr.SetDictionary("rus");
+                        Translatоr.TranslateToMorse(fReader);
+                    }
+                    if (i == 4)
+                    {
+                        Translatоr.SetDictionary("rus");
+                        Translatоr.TranslateFromMorse(fReader);
+                    }
+                }                   
+            }
+        }
+    }
+}
---
+++ Morse_Translator/Translatоr.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Morse_Translator
+{
+    public static class Translatоr
+    {
+        private static readonly Dictionary<char, string> To_Morse = new Dictionary<char, string> { };
+        private static readonly Dictionary<string, char> From_Morse = new Dictionary<string, char> { };
+
+        public static void SetDictionary(string language)
+        {
+            if (language == "rus")
+            {
+                using (StreamReader fstream = new StreamReader("dictionary_rus.txt"))
+                {
+                    string str = "";
+                    while (!fstream.EndOfStream)
+                    {
+                        str = fstream.ReadLine();
+                        To_Morse.Add(str[0], str.Substring(1, str.Length - 1));
+                        From_Morse.Add(str.Substring(1, str.Length - 1), str[0]);
+                    }
+                }
+            }
+
+            if (language == "eng")
+            {
+                using (StreamReader fstream = new StreamReader("dictionary_eng.txt"))
+                {
+                    string str = "";
+                    while (!fstream.EndOfStream)
+                    {
+                        str = fstream.ReadLine();
+                        To_Morse.Add(str[0], str.Substring(1, str.Length - 1));
+                        From_Morse.Add(str.Substring(1, str.Length - 1), str[0]);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// перевод С английского и русского на Морзе и наоборот
+        /// </summary>
+        /// <param name="i"></param>
+
+        public static void TranslateToMorse(StreamReader fReader)
+        {
+            char in_str;
+
+            using (StreamWriter fWriter = new StreamWriter("output.txt", false)) { }
+
+            while (!fReader.EndOfStream)
+            {
+                in_str = Convert.ToChar(fReader.Read());
+
+                in_str = Convert.ToChar(Convert.ToString(in_str).ToUpper());
+
+                using (StreamWriter fWriter = new StreamWriter("output.txt", true))
+                {
+                    try
+                    {
+                        fWriter.Write(To_Morse[Convert.ToChar(in_str)] + " ");
+                    }
+                    catch
+                    {
+                        fWriter.Write("   ");
+                    }
+
+                }
+            }
+        }
+
+        public static void TranslateFromMorse(StreamReader fReader)
+        {
+            string in_str = "";
+            char symbol;
+            int countProbel = 0;
+
+            using (StreamWriter fWriter = new StreamWriter("output.txt", false)) { }
+
+            while (!fReader.EndOfStream)
+            {
+                while ((symbol = Convert.ToChar(fReader.Read())) != ' ')
+                {
+                    in_str += symbol;
+                }
+
+                countProbel++;
+
+                using (StreamWriter fWriter = new StreamWriter("output.txt", true))
+                {
+                    try
+                    {
+                        fWriter.Write(From_Morse[in_str]);
+                        countProbel = 0;
+                    }
+                    catch
+                    {
+                        if (countProbel > 1)
+                        {
+                            fWriter.Write(" ");
+                            countProbel = 0;
+                        }
+                    }
+                }
+
+                in_str = "";
+            }
+        }
+    }
+}
